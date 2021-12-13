5a8d8f6c9d8926212f432122973b2fef555fb6dd
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2021-12-13 19:42:50 +0300

    feat: configure project

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+import express from 'express'
+
+const PORT = 5000
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({ hello: 'world' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
